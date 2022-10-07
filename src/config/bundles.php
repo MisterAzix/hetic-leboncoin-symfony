681485294e1d58e47c846b8f5a22959ee2839c9e
username <email>
2022-10-07 17:33:43 +0200

    ETQ dev JV installer des dependences

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
