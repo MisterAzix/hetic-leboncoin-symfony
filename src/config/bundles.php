02017d35358550dc0b0e4ece276e7e1632977a51
Justin katasi <108656766+justinDev91@users.noreply.github.com>
2022-10-08 19:35:37 +0200

    Installer des dépendances (#9)
    * wip
    
    * ETQ dev JV installer des dependences
    
    Co-authored-by: username <email>

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
