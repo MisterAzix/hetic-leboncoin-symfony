2ae6933e90a54ca7316b32305aeeda3e47cad816
Julian Laballe <julian.laballe@mmibordeaux.com>
2022-10-24 22:42:00 +0200

    feat: create navbar

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. global.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
     .addEntry('countVote', './assets/countVote.js')
