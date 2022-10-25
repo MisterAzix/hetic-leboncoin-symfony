28df2e28f32d5879e12a571f5899ff04baa0c538
Justin katasi <108656766+justinDev91@users.noreply.github.com>
2022-10-25 13:23:27 +0200

    fixe user collection bundle (#21)

--- src/src/Entity/User.php
+++ src/src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
