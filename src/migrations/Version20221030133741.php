5040b78461862572028047543d571c30971d49ba
MisterAzix <flizi@live.fr>
2022-10-30 14:42:24 +0100

    chore: fix vote migrations

---
+++ src/migrations/Version20221030133741.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221030133741 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, from_user_id VARCHAR(255) NOT NULL, to_user_id VARCHAR(255) NOT NULL, direction VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tag DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE user ADD votes INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE vote');
+        $this->addSql('ALTER TABLE user DROP votes');
+        $this->addSql('ALTER TABLE tag ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL');
+    }
+}
